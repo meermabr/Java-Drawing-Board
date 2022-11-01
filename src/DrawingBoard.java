847564b37ec600eadd1e8f4f2c0ac389100f2138
Brian Meermans <brian@meermans.com>
2022-11-01 14:54:07 -0400

    Task 0 and 1 finished

--- src/DrawingBoard.java
+++ src/DrawingBoard.java
@@ -4,33 +4,47 @@ public class DrawingBoard {
     private PaintingTool paint = new PaintingTool();
 
     public void initialLaunch() {
-        // 1.1 Change the background color
+        // 0.1 Change the background color
         screen.setBackgroundColor(Color.ORANGE);
 
-        // 1.2 Identify the corners
+        // 0.2 Identify the corners
         paint.setFillColor(Color.RED);
         paint.drawCircle(0, 0, 20);
-        paint.setFillColor(Color.GREEN);
-        paint.drawCircle(0, screen.getHeight(), 20);
-        paint.setFillColor(Color.BLUE);
-        paint.drawCircle(screen.getWidth(), 0, 20);
-        paint.setFillColor(Color.MAGENTA);
-        paint.drawCircle(screen.getWidth(), screen.getHeight(), 20);
 
-        // 1.3 Draw an interior surface
+        paint.setFillColor(Color.GREEN);
+        paint.drawCircle(0, 400, 20);
+
+        paint.setFillColor(Color.BLUE);
+        paint.drawCircle(600, 0, 20);
+
+        paint.setFillColor(Color.MAGENTA);
+        paint.drawCircle(600, 400, 20);
+
+        // 0.3 Draw an interior surface
         paint.setFillColor(Color.WHITE);
-        paint.drawRect(30, 30, screen.getWidth() - 60, screen.getHeight() - 60);
+        paint.drawRect(30, 30, 540, 340);
 
     }
 
     public void update() {
+        // 1.1 Draw a line anywhere
+        paint.setLineColor(Color.CYAN);
+        // paint.drawLine(0, 0, 600, 400);
 
+        // 1.2 Generate random color values from 0 to 255 for red, green, and blue lines.
+        paint.setLineColor(
+                Generator.randomInt(255),
+                Generator.randomInt(255),
+                Generator.randomInt(255)
+        );
 
+        // 1.3 Use the x and y coordinates of the mouse
+        // paint.drawLine(mouseInfo.getX(), mouseInfo.getY(), 0, 0);
 
-        paint.setLineColor(Generator.randomInt(255), Generator.randomInt(255), Generator.randomInt(255));
+        // 1.4 Use the very center of the screen
+        // paint.drawLine(mouseInfo.getX(), mouseInfo.getY(), 300, 200);
+
+        // 1.5 Use the center of the screen without using the actual coordinates
         paint.drawLine(mouseInfo.getX(), mouseInfo.getY(), screen.getWidth() / 2, screen.getHeight() / 2);
     }
-
-
-
 }
